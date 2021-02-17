a55dc3111e120f755663d7355873396ff659ad8d
alcor146 <74672415+alcor146@users.noreply.github.com>
2021-02-17 22:18:50 +0200

    Add files via upload

--- demux.v
+++ demux.v
@@ -45,97 +45,180 @@ module demux #(
 		output reg [SYS_DWIDTH - 1 : 0] 	data2_o,
 		output reg     						valid2_o
     );
-	//daca crapa, reset in always
 	
-	reg [SYS_DWIDTH - 1 : 0] data0_o_next, data1_o_next, data2_o_next;
-	reg [5:0] i;
-	reg valid0_o_next, valid1_o_next, valid2_o_next, valid_i_next;
+	
+	// TODO: Implement DEMUX logic
+	
+	reg [3:0] state = 0;
+	
+	reg [SYS_DWIDTH - 1 : 0] 	dataPart1;
+	reg [SYS_DWIDTH - 1 : 0] 	dataPart2;
+	reg [SYS_DWIDTH - 1 : 0] 	dataPart3;
+	reg [SYS_DWIDTH - 1 : 0] 	dataPart4;
+	// folosite pentru a imparti data_i in 4
+	
+	always @(posedge clk_sys) begin
+		if(!rst_n) begin
+			state <= 0;
+			
+			data0_o <= 0;
+			data1_o <= 0;
+			data2_o <= 0;
+			
+			valid0_o <= 0;
+			valid1_o <= 0;
+			valid2_o <= 0;
+			
+			// eventual si dataParts <= 0
+		end
+		
+		case(state)
+			'd0: begin
+				if(valid_i) 
+				// daca primim date, intram in ciclul de stari
+					state <= 1;
+			end
+			
+			'd1: begin
+			// spargem chunk-ul de date in 4
+			
+				dataPart1 <= data_i[MST_DWIDTH-1 : MST_DWIDTH-SYS_DWIDTH];
+				dataPart2 <= data_i[MST_DWIDTH-SYS_DWIDTH-1 : MST_DWIDTH-2*SYS_DWIDTH];
+				dataPart3 <= data_i[MST_DWIDTH-2*SYS_DWIDTH-1 : MST_DWIDTH-3*SYS_DWIDTH];
+				dataPart4 <= data_i[MST_DWIDTH-3*SYS_DWIDTH-1 : MST_DWIDTH-4*SYS_DWIDTH];
+				state <= 2;
+			end
+			
+			'd2: begin
+			// mai asteptam in ciclu de ceas
+			
+				state <= 3;
+			end
+			
+			'd3: begin
+			// incepem afisarea deodata cu urmatorul ciclu de ceas
+			
+				state <= 4;
+				case(select)
+					'd0: begin
+						valid0_o <= 1;
+						data0_o <= dataPart1;
+					end
+					
+					'd1: begin
+						valid1_o <= 1;
+						data1_o <= dataPart1;
+					end
+					
+					'd2: begin
+						valid2_o <= 1;
+						data2_o <= dataPart1;
+					end
+				endcase
+			end
+			
+			'd4: begin
+			// continuam afisarea 
+			
+				state <= 5;
+				case(select)
+					'd0: begin
+						data0_o <= dataPart2;
+					end
+					
+					'd1: begin
+						data1_o <= dataPart2;
+					end
+					
+					'd2: begin
+						data2_o <= dataPart2;
+					end
+				endcase
+			end
+			
+			'd5: begin
+			// continuam afisarea (a 3-a parte din datele de intrare)
+			
+				state <= 6;
+				case(select)
+					'd0: begin
+						data0_o <= dataPart3;
+					end
+					
+					'd1: begin
+						data1_o <= dataPart3;
+					end
+					
+					'd2: begin
+						data2_o <= dataPart3;
+					end
+				endcase
+			end
+			
+			'd6: begin
+			// afisam ultima parte din datele primite
+			
+				case(select)
+					'd0: begin
+						data0_o <= dataPart4;
+					end
+					
+					'd1: begin
+						data1_o <= dataPart4;
+					end
+					
+					'd2: begin
+						data2_o <= dataPart4;
+					end
+				endcase
+				
+				if(valid_i) begin
+				// daca intre timp am primit alte date, ne intoarcem in starea in care incepem afisarea lor
+				
+					dataPart1 <= data_i[MST_DWIDTH-1 : MST_DWIDTH-SYS_DWIDTH];
+					dataPart2 <= data_i[MST_DWIDTH-SYS_DWIDTH-1 : MST_DWIDTH-2*SYS_DWIDTH];
+					dataPart3 <= data_i[MST_DWIDTH-2*SYS_DWIDTH-1 : MST_DWIDTH-3*SYS_DWIDTH];
+					dataPart4 <= data_i[MST_DWIDTH-3*SYS_DWIDTH-1 : MST_DWIDTH-4*SYS_DWIDTH];
+					state <= 3;
+				end
+				else begin
+				// nu am primit alte date de intrare
+				
+					state <= 7;
+				end
+				
+			end
+			
+			'd7: begin
+			// stopam orice afisare
+			
+				state <= 0;
+				case(select)
+					'd0: begin
+						valid0_o <= 0;
+						data0_o <= 0;
+					end
+					
+					'd1: begin
+						valid1_o <= 0;
+						data1_o <= 0;
+					end
+					
+					'd2: begin
+						valid2_o <= 0;
+						data2_o <= 0;
+					end
+				endcase
+				
+				
+				
+			end
+			
+			
+		endcase
+		
+	end
 	
 	
 
-	always@(posedge clk_mst) begin
-	
-		if(!rst_n)begin
-				data0_o <= 0;
-				data1_o <= 0;
-				data2_o <= 0;
-				
-				valid0_o <= 0;
-				valid1_o <= 0;
-				valid2_o <= 0;
-			end
-			
-			else begin
-			
-			valid_i_next <= valid_i;
-	
-			end
-	end
-	
-	always@(posedge clk_sys) begin
-	
-		if(!rst_n)begin
-				data0_o <= 0;
-				data1_o <= 0;
-				data2_o <= 0;
-				
-				valid0_o <= 0;
-				valid1_o <= 0;
-				valid2_o <= 0;
-			end
-			
-			else begin
-			
-			data0_o <= data0_o_next;
-			data1_o <= data1_o_next;
-			data2_o <= data2_o_next;
-			
-			valid0_o <= valid0_o_next;
-			valid1_o <= valid1_o_next;
-			valid2_o <= valid2_o_next;
-			end
-	end
-	
-	
-	always @(*)begin
-	
-	
-	for(i=0; i<4; i=i+1)begin
-		case(select)
-				2'b00: begin
-					if(valid_i_next)begin
-						data0_o_next <= data_i[(3-i)*8 +: 8];
-						valid0_o_next <= 1;
-					end
-				end
-				
-				2'b01: begin
-					if(valid_i_next)begin
-						data1_o_next <= data_i[(3-i)*8 +: 8];
-						valid1_o_next <= 1;
-					end
-				end
-				
-				2'b10: begin
-					if(valid_i_next)begin
-						data2_o_next <= data_i[(3-i)*8 +: 8];
-						valid2_o_next <= 1;
-					end
-				end
-				
-				/*default: begin
-					valid_o <= 0;
-					data_o <= 0;
-				end*/
-			endcase
-	
-	end
-	
-	
-	
-	
-	end
-	
-	
-	
 endmodule
